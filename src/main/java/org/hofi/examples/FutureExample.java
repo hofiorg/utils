600d97817132fea57ce7f6cc7b2ac4f6a02356b9
Christian Hofmann <hofiorg@googlemail.com>
2017-09-06 13:55:03 +0200

    FutureExample

---
+++ src/main/java/org/hofi/examples/FutureExample.java
@@ -0,0 +1,41 @@
+package org.hofi.examples;
+
+import java.util.concurrent.*;
+
+import static java.lang.System.out;
+
+public class FutureExample {
+
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    new FutureExample();
+  }
+
+  private FutureExample() throws ExecutionException, InterruptedException {
+
+    long millis = System.currentTimeMillis();
+
+    Future<String> myfuture = futureFunc();
+
+    Thread.sleep(500);
+
+    out.println("hello world 1");
+
+    String value = myfuture.get();
+
+    out.println(value);
+
+    pool.shutdown();
+
+    out.println(System.currentTimeMillis() - millis + " ms");
+  }
+
+  private final ExecutorService pool = Executors.newFixedThreadPool(10);
+
+  private Future<String> futureFunc() {
+    return pool.submit(() -> {
+      out.println("hello world 2");
+      Thread.sleep(500);
+      return "hello world 3";
+    });
+  }
+}
