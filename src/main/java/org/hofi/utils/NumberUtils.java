0040b006f466e7d740504472929149a425a2a56a
hofiorg <hofiorg@googlemail.com>
2017-12-16 23:28:28 +0100

    set default locale

--- src/main/java/org/hofi/utils/NumberUtils.java
+++ src/main/java/org/hofi/utils/NumberUtils.java
@@ -2,14 +2,17 @@ package org.hofi.utils;
 
 import java.text.DecimalFormat;
 import java.text.ParseException;
+import java.util.Locale;
 
 public class NumberUtils {
   public static String formatNumber(Number number) {
+    Locale.setDefault(new Locale("de", "DE"));
     DecimalFormat df2 = new DecimalFormat( "#,###,###,##0.00" );
     return df2.format(number);
   }
 
   public static Number parseNumber(String s) throws ParseException {
+    Locale.setDefault(new Locale("de", "DE"));
     DecimalFormat df2 = new DecimalFormat( "#,###,###,##0.00" );
     return df2.parse(s);
   }
